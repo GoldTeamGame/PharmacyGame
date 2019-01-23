b04fa39a3926a6c163a15d55da9b03d47867eebb
AlexJacks1 <definatelynotacat@gmail.com>
2019-01-23 16:17:38 -0600

    Started Customer Class and Globals

---
+++ Assets/Scripts/Globals/Globals_Customer.cs
@@ -0,0 +1,15 @@
+﻿// File: Globals_Customer
+// Description: Holds Customers
+
+using UnityEngine;
+
+public static class Globals_Customer
+{
+    public const int MAX_CUSTOMERS = 100;
+
+    public static int currentID = 0;
+    public static GameObject[] customer = new GameObject[MAX_CUSTOMERS];
+    public static Customer[] customerData = new Customer[MAX_CUSTOMERS];
+
+    public static string[] name = { "Alex", "Dylan", "Jon", "Ross" };
+}
