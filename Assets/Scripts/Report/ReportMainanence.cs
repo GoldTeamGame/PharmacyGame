79159c38c87373e8185dddf051fa697ca9b3593c
jphearn2 <johearn@siue.edu>
2019-03-20 09:21:52 -0500

    page swapping works now

--- Assets/Scripts/Report/ReportMainanence.cs
+++ Assets/Scripts/Report/ReportMainanence.cs
@@ -8,11 +8,27 @@ public class ReportMainanence : MonoBehaviour {
 	public static string selectedText;
 	public static Text selectedObj;
 	public static string selectedType;
+	public GameObject test;
+	public GameObject screen1;
+	public GameObject screen2;
 	
+
+	public void pageLeft(){
+		if(screen2.active){
+			screen2.SetActive(false);
+			screen1.SetActive(true);
+		}
+	}
+	public void pageRight(){
+		if(screen1.active){
+			screen1.SetActive(false);
+			screen2.SetActive(true);
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
