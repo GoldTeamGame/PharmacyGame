4244e96df4edda1d4f5ac24472a79430ad6e937b
dylancyphers <dylan.cyphers3@gmail.com>
2019-04-10 13:46:06 -0500

    added report scene and its scripts to dev

---
+++ Assets/Scripts/Report/ReportMainanence.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReportMainanence : MonoBehaviour {
+
+	public static string selectedText;
+	public static Text selectedObj;
+	public static string selectedType;
+	public GameObject test;
+	public GameObject screen1;
+	public GameObject screen2;
+	
+
+	public void pageLeft(){
+		if(screen2.active){
+			screen2.SetActive(false);
+			screen1.SetActive(true);
+		}
+	}
+	public void pageRight(){
+		if(screen1.active){
+			screen1.SetActive(false);
+			screen2.SetActive(true);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
